703e2ef971e4335e70e41c69e2d325c04e7ab4d8
Sophia Angelina <161032086+sophiaasb@users.noreply.github.com>
2025-02-06 22:26:18 +0000

    main

--- aula1/Concatenar.java
+++ aula1/Concatenar.java
@@ -4,18 +4,18 @@ public class Concatenar {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Digite uma palavra: ");
         String palavra1 = sc.next();
 
         System.out.print("Digite uma palavra: ");
         String palavra2 = sc.next();
 
-        /* 
-        System.out.println("Resultado = " +x+ " metros");
+        /* System.out.println("Resultado = " +x+ " metros");
         System.out.println("Resultado = %.2f metros%n", x); */
 
         System.out.println("A junção das palavras foram: " +palavra1+ " " +palavra2);
+
+        sc.close();
 
     }
 }
